56393891fd01592324f3290dc086dc5f88c405ef
winchestor2001 <sbexruz2001@gmail.com>
2025-03-03 23:36:04 +0500

    Fix: schedule mailing text, video, animation

--- database/models.py
+++ database/models.py
@@ -56,6 +56,6 @@ class Product(BaseModel):
 class ScheduledPost(BaseModel):
     post_type = CharField()
     content = TextField()
-    file_id = TextField()
+    file_id = TextField(null=True)
     schedule_time = DateTimeField()
     buttons = TextField(null=True)
